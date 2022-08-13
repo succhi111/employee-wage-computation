65c10ea278d711c78697bd94402f358d893b7d49
succhi111 <suchendramishra396@gmail.com>
2022-08-13 16:03:52 +0530

    UC3_computing_part_time

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,13 +1,20 @@
+
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int wagePerHour = 20;
-        int fullDayHour = 8;
+        int fullTimeHour = 8;
+        int partTimeHour = 4;
         int dailyWage = 0;
-        int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if(employeeCheck == 1) {
-            dailyWage = fullDayHour * wagePerHour;
-            System.out.println("Employee is present");
+
+        int employeeCheck = (int) (Math.random() * ((2 + 0) + 1));
+
+        if (employeeCheck == 2) {
+            dailyWage = fullTimeHour * wagePerHour;
+            System.out.println("Employee is present full time");
+        } else if (employeeCheck == 1) {
+            dailyWage = partTimeHour * wagePerHour;
+            System.out.println("Employee is present half time");
         } else {
             System.out.println("Employee is absent");
         }
